cf615df22f9b2a79ca95005f7b57a7fc0dc90455
Heorhii <kutafin01@gmail.com>
2021-10-01 13:44:38 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+let result = num1 * num2;
+let result1 = num1 / num2;
+let result2 = +num1 + +num2;
+let result3 = num1 - num2;
+alert(result,result1,result2,result3);
+console.log(`Результат:${num1}*${num2}=${result}`);
+console.log(`Результат:${num1}/${num2}=${result1}`);
+console.log(`Результат:${num1}+${num2}=${result2}`);
+console.log(`Результат:${num1}-${num2}=${result3}`);
